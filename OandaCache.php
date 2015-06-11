9a73b36b05035a4d201a3270b02dd79dacf8cfa6
Will <will.whitty.arbeit@gmail.com>
2015-06-12 01:13:13 +0300

    Cleanup up OandaCache.php, now uses global loader for account config file

--- OandaCache.php
+++ OandaCache.php
@@ -1,84 +1,66 @@
 <?php
 
-  /*
+/*
 
-    Copyright 2015 William Whitty
-    will.whitty.arbeit@gmail.com
+  Copyright 2015 William Whitty
+  will.whitty.arbeit@gmail.com
 
-    Licensed under the Apache License, Version 2.0 (the 'License');
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the 'License');
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an 'AS IS' BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an 'AS IS' BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 
-  */
+*/
 
-  // This file kept seperate for debugging purposes.
-  // To use OandaCache on your system, see readme.txt
+// This file kept seperate for debugging purposes.
+// To use OandaCache on your system, see readme.txt
 
-  //error_reporting(E_ALL);
-  //ini_set('display_errors', 1);
 set_time_limit(0);
 
 if (defined('OANDA_CACHE_INDEX') === FALSE) {
-  define('OANDA_CACHE_INDEX', TRUE);
+    define('OANDA_CACHE_INDEX', TRUE);
 	
-  //OandaWrap and setup
-  require '../OandaWrap/OandaWrap.php';
+    //OandaWrap and setup
+    require 'OandaWrap.php';
 
-  $configDir = './config/';
-  $config    = $configDir . 'account.cfg';
-	
-  //Check to see if our config file exists
-  if (file_exists($config) === FALSE)
-	  throw new Exception('Failed to open Config,
-	Config requires:
-		ACCOUNT_KEY=YOUR_API_KEY
-		ACCOUNT_NUM=YOUR_ACCOUNT_NUM
-		ACCOUNT_TYP=Demo //(Or "Live")
-	');
+    //Local source files
+    require 'source/Server.php';
 
-  //Set up the array in which to store the acount information
-  $accInfo = array('ACCOUNT_TYP' => '', 'ACCOUNT_KEY' => '', 'ACCOUNT_NUM' => '');
-  
-  //Load the values from the config file
-  foreach (str_getcsv(file_get_contents($config), "\n") as $line)
-	  //If the line is not a comment and contains an '=' sign
-	  if (strpos($line, '=') !== FALSE && strpos($line, '/') === FALSE)
-		//Split the line and analyse
-		if (($data = str_getcsv($line, '=')) && count($data) > 1)
-			//Add the data key to our account Info
-			$accInfo[$data[0]] = $data[1];
-  
-  //Set up OandaWrap with the values from the config
-  if (OandaWrap::setup(@$accInfo['ACCOUNT_TYP'], @$accInfo['ACCOUNT_KEY'], @$accInfo['ACCOUNT_NUM'], FALSE) === FALSE)
+    //Initialize the OandaCache
+    OandaCache::setup();
+
+    echo "Loading account config:\n\n";
+    OandaCache::global_load('./config/account.cfg');
+    
+    $accountId   = OandaCache::global_value('ACCOUNT_KEY');
+    $accountKey  = OandaCache::global_value('ACCOUNT_NUM');
+    $accountType = OandaCache::global_value('ACCOUNT_TYP');
+    
+    //Set up OandaWrap with the values from the config
+    if (OandaWrap::setup($accountType, $accountId, $accountKey, FALSE) === FALSE)
 	{ echo "\nOandaWrap failed to initialize, check your API Key in ./config/accounts.cfg\n"; die(1); }
 
-  //Local source files
-  require 'source/Server.php';
-
-  //Initialize the OandaCache
-  OandaCache::setup();
-
-  //Add each of the pairs from the pairs.cfg
-  echo "Loading pair list...\n";
-  OandaCache::pairs_add(str_getcsv(file_get_contents('./config/pairs.cfg'), "\n"));
+    //Add each of the pairs from the pairs.cfg
+    echo "\nLoading pair list...\n";
+    OandaCache::pairs_add(str_getcsv(file_get_contents('./config/pairs.cfg'), "\n"));
         
-  //Add each of the times from the times.cfg
-  echo "Loading time settings...\n";
-  OandaCache::times_add(str_getcsv(file_get_contents('./config/times.cfg'), "\n"));
+    //Add each of the times from the times.cfg
+    echo "Loading time settings...\n";
+    OandaCache::times_add(str_getcsv(file_get_contents('./config/times.cfg'), "\n"));
         
-  //Load our general config
-  echo "Loading general config file... \n\n";
-  OandaCache::global_load('./config/config.cfg');
+    //Load our general config
+    echo "Loading general config file... \n\n";
+    OandaCache::global_load('./config/config.cfg');
 
-  echo "\nStarting stream... \n";
-  //Start streaming, argument is time delay between refresh
-  OandaCache::stream(5);
+    echo "\nStarting stream... \n";
+
+    //Start streaming, argument is time delay between refresh
+    OandaCache::stream(5);
 }
--- README.md
+++ README.md
@@ -12,20 +12,7 @@
            However please see fit to modify OandaCache.php to change this location.
 
     # Setup
-
-        Config files can be opened with a simple text editing program.
-    
-        You need to put your Oanda login information into the file:
-        OandaCache/config/account.cfg
-
-        Maximum number of candles, timezone etc can be set:
-        OandaCache/config/config.cfg
-
-        Different pairs can be loaded:
-        OandaCache/config/pairs.cfg
-
-        Different time frames can be saved:
-        OandaCache/config/times.cfg
+        Make sure to edit OandaCache.php to add your Oanda API key and Account ID
 
         Run with "php OandaCache.php"
 
@@ -35,15 +22,11 @@
     The application allows access to these candles in their original JSON format.
     One candle per line (newline denominated "\n")
 
-    The candles come in the format:
-
-    {"time":TIME_IN_SECONDS,"volume":VOLUME_AS_INTEGER,"low":0.96897,"high":0.96899,"open":0.96897,"close":0.968975}
-
 # Future
 
     I would like to incorporate reloading the executable, or rescanning directory.
     In this way the user could specify which repositary to load on demand.
 
     Now its fire and forget.
-    
+
    Hope you enjoy, let me know if you have issues.
